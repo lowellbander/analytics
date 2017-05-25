7611d5f99f2fcd63b8019484d13f38b7e4360efc
Lowell Bander <lowellb@fb.com>
2017-05-25 11:36:02 -0700

    point sdk at my sandbox

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 5000;
 console.log('Listening on port ', port);
